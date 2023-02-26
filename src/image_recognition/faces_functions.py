00746e68f28b2da2abec8876e5a44ed1ca931dd1
Mohamad Khawam <55813440+dominusmars@users.noreply.github.com>
2023-02-26 17:01:46 -0500

    bugs

--- src/image_recognition/faces_functions.py
+++ src/image_recognition/faces_functions.py
@@ -1,18 +1,23 @@
 import cv2
 
+
 # Load the classifier for profile face detection
-face_cascade = cv2.CascadeClassifier('/xml/haarcascade_profileface.xml')
+face_cascade = cv2.CascadeClassifier(
+    "/Users/mohamadkhawam/Code/HackerRU_NJtransit_2023/src/image_recognition/haarcascade.xml"
+)
 
 
 def find_bodies(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect lower body in the frame
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=1,
-                                           minSize=(10, 20), maxSize=(3000, 6000))
+    faces = face_cascade.detectMultiScale(
+        gray, scaleFactor=1.3, minNeighbors=1, minSize=(10, 20), maxSize=(3000, 6000)
+    )
     return faces
 
+
 def draw_bodies_on_frame(frame, bodies):
-    for (x, y, w, h) in bodies:
+    for x, y, w, h in bodies:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
     return frame
--- src/init.py
+++ src/init.py
@@ -5,6 +5,7 @@ from client import client
 from image_recognition import faces_functions
 import cv2
 import numpy as np
+import math
 
 url = "http://192.168.212.151:5000"
 
@@ -18,15 +19,13 @@ passengers = 0
 passengers_this_round = -1
 tickets_this_round = 0
 average_this_round = []
-parse_amount = 20
+parse_amount = 40
 
 cl = client.Client(url)
 
 
 # Define a function to reset variables for a new round
 def reset_round():
-    global average_this_round
-    average_this_round = -1
     global frames_parse
     frames_parse = 0
     global passengers_this_round
@@ -54,15 +53,18 @@ def main():
     global cl
     init()
     # Loop over each frame from the video stream
-    for frame in cl.get_video_stream(url):
+    for frame in cl.get_video_stream():
         # Find bodies in the current frame using image recognition
-        bodies = faces_functions.find_bodies(frame)
+        if not get_ticket:
+            bodies = faces_functions.find_bodies(frame)
 
-        # Draw the bodies on the frame for visualization
-        final_frame = faces_functions.draw_bodies_on_frame(frame, bodies)
+            # Draw the bodies on the frame for visualization
+            final_frame = faces_functions.draw_bodies_on_frame(frame, bodies)
 
-        # Display the final frame
-        cv2.imshow("Video Stream", final_frame)
+            # Display the final frame
+            cv2.imshow("Video Stream", final_frame)
+        else:
+            cv2.imshow("Video Stream", frame)
 
         # Check if it's time to move forward
         if move_froward:
@@ -75,16 +77,17 @@ def main():
 
         # Check if it's time to detect people
         if detect_people:
+            print("scanning ...")
             cl.display("Scanning ...")
 
             # Keep track of the number of bodies detected in each frame
             if len(average_this_round) >= parse_amount:
                 detect_people = False
                 get_ticket = True
-                passengers_this_round = np.average(average_this_round)
+                passengers_this_round = math.ceil(np.average(average_this_round))
                 passengers = passengers + passengers_this_round
-                print("Passengers detected:  " + passengers_this_round)
-                print("Total of Passengers: " + passengers)
+                print("Passengers detected:", passengers_this_round)
+                print("Total of Passengers:", passengers)
                 continue
             number_of_bodies = len(bodies)
             average_this_round.append(number_of_bodies)
@@ -96,14 +99,13 @@ def main():
             if qr_functions.identify_qr_codes(frame):
                 get_ticket = False
                 if next_step_move:
-                    cl.display("Thank you, Have a safe trip", 1)
+                    cl.display("Thank you", 1)
                     time.sleep(3)
                     move_froward = True
                 else:
-                    cl.display("Thank you, Have a safe trip", 1)
+                    cl.display("Thank you", 1)
                     time.sleep(3)
-                    cl.moveCamera(turn_camera_180)
-                    turn_camera_180 = not turn_camera_180
+                    cl.moveCamera()
                     reset_round()
                     detect_people = True
 
